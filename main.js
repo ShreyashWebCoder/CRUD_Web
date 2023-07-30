6bebf76fb3921207f9515ee344b4c51e2d577f50
Shreyash Web Developer <131770664+ShreyashWebCoder@users.noreply.github.com>
2023-07-30 17:54:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,419 @@
+
+let movies = [
+    {
+        id: 1,
+        "title": "Baazigar",
+        "year": "1993",
+        "genres": [
+            "Crime",
+            "Drama",
+            "Musical"
+        ],
+        "ratings": [
+            4,
+            3,
+            5,
+            8,
+            5,
+            5,
+            3,
+            10,
+            9,
+            8,
+            5,
+            9,
+            10,
+            5,
+            8,
+            9,
+            1,
+            1,
+            5,
+            7,
+            7,
+            10,
+            7,
+            8,
+            1,
+            6,
+            6,
+            5,
+            10,
+            9
+        ],
+        "poster": "MV5BMTc3OTE1MDAxNV5BMl5BanBnXkFtZTgwNjM1NDk5NTE@.V1_SY250_CR0,0,187,250_AL.jpg",
+        "contentRating": "",
+        "duration": "PT175M",
+        "releaseDate": "1993-11-12",
+        "averageRating": 0,
+        "originalTitle": "",
+        "storyline": "Widowed Madan Chopra lives a very wealthy lifestyle with two daughters, Seema and Priya. His passion is car racing, and realizing that he is not young anymore, has his last race and wins - only to find out that another competitor, Vicky Malhotra, let him win. Madan befriends Vicky, introduces him to Priya and invites him to work for him in Bombay. Vicky and Priya are attracted to each other, much to Madan's delight and he announces their marriage. Before that could happen, Seema kills herself by throwing herself down from the roof of a multi-storied building. While Madan wants to close this case due to embarrassment, Seema meets with her college friend, Police Inspector Karan, and together they begin to unravel a plot that may well end Madan and the lives of others as well.                Written by\nrAjOo (gunwanti@hotmail.com)",
+        "actors": [
+            "Shah Rukh Khan",
+            "Rakhee Gulzar",
+            "Kajol"
+        ],
+        "imdbRating": 7.8,
+        "posterurl": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTc3OTE1MDAxNV5BMl5BanBnXkFtZTgwNjM1NDk5NTE@._V1_SY250_CR0,0,187,250_AL_.jpg"
+    },
+
+    {
+        id: 2,
+        "title": "24",
+        "year": "2016",
+        "genres": [
+            "Action",
+            "Romance",
+            "Sci-Fi"
+        ],
+        "ratings": [
+            5,
+            1,
+            5,
+            1,
+            3,
+            9,
+            3,
+            2,
+            8,
+            6,
+            7,
+            2,
+            9,
+            9,
+            9,
+            10,
+            6,
+            5,
+            1,
+            10,
+            7,
+            4,
+            5,
+            8,
+            6,
+            2,
+            4,
+            10,
+            9,
+            6
+        ],
+        "poster": "MV5BZmYyNWRmNjUtODg5OC00YmRjLTkwYWMtMjY5NjA5ZjhkNDg3XkEyXkFqcGdeQXVyNDA1NTY2Nzk@.V1_SY500_CR0,0,333,500_AL.jpg",
+        "contentRating": "",
+        "duration": "PT164M",
+        "releaseDate": "2016-05-06",
+        "averageRating": 0,
+        "originalTitle": "",
+        "storyline": "A Sci-Fi family revenge drama happening between a scientist, his evil brother and the scientist's son, over a time travel gadget.                Written by\nNaveen Sankaran",
+        "actors": [
+            "Suriya",
+            "Samantha Ruth Prabhu",
+            "Nithya Menon"
+        ],
+        "imdbRating": 8.0,
+        "posterurl": "https://images-na.ssl-images-amazon.com/images/M/MV5BZmYyNWRmNjUtODg5OC00YmRjLTkwYWMtMjY5NjA5ZjhkNDg3XkEyXkFqcGdeQXVyNDA1NTY2Nzk@._V1_SY500_CR0,0,333,500_AL_.jpg"
+    },
+
+    {
+        id: 3,
+        "title": "Jodhaa Akbar",
+        "year": "2008",
+        "genres": [
+            "Action",
+            "Adventure",
+            "Biography"
+        ],
+        "ratings": [
+            7,
+            2,
+            3,
+            8,
+            6,
+            1,
+            4,
+            2,
+            1,
+            10,
+            6,
+            3,
+            7,
+            4,
+            4,
+            1,
+            3,
+            9,
+            5,
+            10,
+            2,
+            6,
+            9,
+            1,
+            3,
+            7,
+            9,
+            7,
+            3,
+            9
+        ],
+        "poster": "MV5BMTI1ODQ2MDIxMl5BMl5BanBnXkFtZTcwNTc3Mzk1MQ@@.V1_SY361_SX250_AL.jpg",
+        "contentRating": "",
+        "duration": "PT213M",
+        "releaseDate": "2008-02-15",
+        "averageRating": 0,
+        "originalTitle": "",
+        "storyline": "Jodhaa Akbar is a sixteenth century love story about a marriage of alliance that gave birth to true love between a great Mughal Emperor, Akbar and a Rajput princess, Jodhaa. Politically, success knew no bounds for Emperor Akbar, After having secured the Hindu Kush, he furthered his realm by conquest until his empire extended from Afghanistan to the Bay of Bengal, and from the Himalayas to the Godhavari River. Through a shrewd blend of tolerance, generosity and force, Akbar won the allegiance of the Rajputs, the most belligerent Hindus. But little did Akbar know that when he married Jodhaa, a fiery Rajput princess, in order to further strengthen his relations with the Rajputs, he would in turn be embarking upon a new journey - the journey of true love. The daughter of King Bharmal of Amer, Jodhaa resented being reduced to a mere political pawn in this marriage of alliance, and Akbar's biggest challenge now did not merely lie in winning battles, but in winning the love of Jodhaa - a ...                Written by\nhEmRaJ (gavin_coolhgr@hotmail.com)",
+        "actors": [
+            "Hrithik Roshan",
+            "Aishwarya Rai Bachchan",
+            "Sonu Sood"
+        ],
+        "imdbRating": 7.6,
+        "posterurl": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTI1ODQ2MDIxMl5BMl5BanBnXkFtZTcwNTc3Mzk1MQ@@.V1_SY361_SX250_AL.jpg"
+    },
+
+    {
+        id: 4,
+        "title": "Wake Up Sid",
+        "year": "2009",
+        "genres": [
+            "Comedy",
+            "Drama",
+            "Romance"
+        ],
+        "ratings": [
+            3,
+            6,
+            7,
+            10,
+            6,
+            7,
+            3,
+            3,
+            1,
+            4,
+            10,
+            2,
+            6,
+            3,
+            6,
+            1,
+            6,
+            7,
+            8,
+            4,
+            10,
+            9,
+            10,
+            4,
+            6,
+            2,
+            5,
+            6,
+            2,
+            4
+        ],
+        "poster": "MV5BMTg2NTk2MjU5NV5BMl5BanBnXkFtZTcwNTY0Nzg4Mg@@.V1_SY489_SX324_AL.jpg",
+        "contentRating": "",
+        "duration": "PT138M",
+        "releaseDate": "2009-10-02",
+        "averageRating": 0,
+        "originalTitle": "",
+        "storyline": "In Mumbai, Sid Mehra is, in the words of his father, an arrogant, spoiled brat. He lives with a doting mother, subservient brother, and a father who covers his expenses and credit card bills. Sid takes his college finals then starts work at his father's business; he lasts less than a week before walking out. At a graduation party, he meets Aisha, newly arrived from Calcutta and set on becoming a writer. He shows her the city and helps her refurbish a rented flat. He asks if she'd like to progress from friend to something more.",
+        "actors": [
+            "Ranbir Kapoor",
+            "Konkona Sen Sharma",
+            "Supriya Pathak"
+        ],
+        "imdbRating": 7.6,
+        "posterurl": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTg2NTk2MjU5NV5BMl5BanBnXkFtZTcwNTY0Nzg4Mg@@.V1_SY489_SX324_AL.jpg"
+    },
+
+]
+
+
+
+// Function of Movie Array 
+
+function displayMovieTable(arr) {
+
+    arr.forEach((movie, index) => {
+
+        let row_data = document.createElement("tr");
+
+        let numbering = document.createElement('td');
+        numbering.append(index + 1);
+        row_data.appendChild(numbering);
+
+        let title = document.createElement('td');
+        title.append(movie.title);
+        row_data.appendChild(title);
+
+        let releaseDate = document.createElement('td');
+        releaseDate.append(movie.releaseDate);
+        row_data.appendChild(releaseDate);
+
+        let genres = document.createElement('td');
+        movie.genres.forEach((genre, index) => {
+            genres.append(genre + " . ")
+        })
+        row_data.appendChild(genres)
+
+        let duration = document.createElement("td");
+        duration.append(movie.duration);
+        row_data.appendChild(duration);
+
+        let imdbRating = document.createElement('td');
+        imdbRating.append(movie.imdbRating);
+        row_data.appendChild(imdbRating);
+
+        let year = document.createElement('td');
+        year.append(movie.year);
+        row_data.appendChild(year);
+
+
+
+
+        let actions = document.createElement('td');
+        actions.classList.add("icons");
+
+
+        let eye = document.createElement('i');
+        eye.classList.add("fa-solid");
+        eye.classList.add("fa-eye");
+        actions.appendChild(eye);
+        eye.onclick = openMovieModal.bind(this, movie.id);
+
+
+
+        let edit = document.createElement('i');
+        edit.classList.add("fa-solid");
+        edit.classList.add("fa-pen-to-square");
+        actions.appendChild(edit);
+
+
+        let trash = document.createElement('i');
+        trash.classList.add("fa-solid");
+        trash.classList.add("fa-trash-can");
+        actions.appendChild(trash);
+
+
+        row_data.appendChild(actions);
+
+
+        document.getElementById("movie_data").appendChild(row_data);
+    })
+}
+displayMovieTable(movies)
+
+
+
+
+// OPEN Modal Function By Eye Button 
+function openMovieModal(movieid) {
+
+    let movie = movies.find((movie, index) => {
+        return movie.id === movieid
+    })
+
+    document.getElementById('movie_name').innerHTML = movie.title;
+    document.getElementById('movie_storyline').innerHTML = movie.storyline;
+    document.getElementById('movie_actors').innerHTML = movie.actors;
+    document.getElementById('movie_releaseDate').innerHTML = movie.releaseDate;
+    document.getElementById('movie_duration').innerHTML = movie.duration;
+    document.getElementById('movie_imdbRating').innerHTML = movie.imdbRating;
+    document.getElementById('movie_genres').innerHTML = movie.genres;
+    document.getElementById('movie_posterurl').src = movie.posterurl;
+
+    document.getElementById('modal').style.display = 'flex';
+
+}
+
+
+// Close Modal Function
+function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+}
+
+
+
+// Function for Movie Filter Open & Closed Filter 
+let filterStatus = true;
+
+function addMovie() {
+
+    if (filterStatus === true) {
+        let ele = document.getElementById("filter_box");
+        ele.style.marginRight = "0%";
+        ele.style.boxShadow = "rgba(0,0,0,.35) 5px 10px 20px";
+        filterStatus = false;
+    }
+    else {
+        let ele = document.getElementById("filter_box");
+        ele.style.marginRight = "-30%%";
+        ele.style.boxShadow = "none";
+        filterStatus = true;
+    }
+}
+
+
+// Add New ReleaseDate Movie 
+function changeReleaseDate() {
+    document.getElementById('add_releaseDate').type = 'date';
+}
+
+// function addMovie(){
+//     document.getElementById("addModal").style.display = 'flex';
+// }
+
+
+
+// // Banner Icons
+
+// let HeartStatus = false;
+
+// function openLike() {
+
+//     if (HeartStatus === false) {
+//         let ele = document.getElementById("heart");
+//         ele.style.marginLeft = "0%";
+//         ele.style.boxShadow = "rgba(0,0,0,.35) 5px 10px 20px";
+//         filterStatus = true;
+//     }
+//     else {
+//         let ele = document.getElementById("filter_box");
+//         ele.style.marginLeft = "-30%%";
+//         ele.style.boxShadow = "none";
+//         filterStatus = false;
+//     }
+// }
+
+
+// // Add Movie
+// function closeModal(modalID){
+//     document.getElementById(modalID).style.display = "none";
+// }
+
+
+
+
+
+
+// OPEN Modal Function By Eye Button
+// function openMovieModal(movieid) {
+
+//     let movie = movies.find((movie, index) => {
+//         return movie.id === movieid
+//     })
+
+//     document.getElementById('movie_name').innerHTML = movie.title;
+//     document.getElementById('movie_storyline').innerHTML = movie.storyline;
+//     document.getElementById('movie_actors').innerHTML = movie.actors;
+//     document.getElementById('movie_releaseDate').innerHTML = movie.releaseDate;
+//     document.getElementById('movie_duration').innerHTML = movie.duration;
+//     document.getElementById('movie_imdbRating').innerHTML = movie.imdbRating;
+//     document.getElementById('movie_genres').innerHTML = movie.genres;
+//     document.getElementById('movie_posterurl').src = movie.posterurl;
+
+//     document.getElementById('modal').style.display = 'flex';
+
+// }
